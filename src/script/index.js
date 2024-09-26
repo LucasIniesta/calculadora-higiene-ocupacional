758a719e077fabe293be6d66f53ca387c71c1b16
LucasIniesta <l.iniesta.94@gmail.com>
2024-09-26 20:04:58 -0300

    feat: Funções para apagar linha e tabela toda

--- src/script/index.js
+++ src/script/index.js
@@ -3,24 +3,26 @@ function search(searchContent) {
         alert('Escreva algo na barra de pesquisa')
         return
     }
+
     
     fetch('analises.json')
-        .then(resposta => resposta.json())
-        .then(json => {
-            const found = json.filter(obj => {
-                return obj.nome.toLowerCase() === searchContent.toLowerCase() || obj.cas === searchContent
-            })
-            if(found.length > 0) {
-                post(found)
-            } else {
-                throw new Error('Não foi possível encontrar sua análise')
-            }
+    .then(resposta => resposta.json())
+    .then(json => {
+        const found = json.filter(obj => {
+            return obj.nome.toLowerCase() === searchContent.toLowerCase() || obj.cas === searchContent
+        })
+        if(found.length > 0) {
+            post(found)
+        } else {
+            throw new Error('Não foi possível encontrar sua análise')
+        }
         })
         .catch(e =>{
             alert(e)
         })
+    
+    document.querySelector('#search').value = ''
 }
-
 
 function post(found) {
     const table = document.querySelector('table')
@@ -31,18 +33,38 @@ function post(found) {
     const vazMax = row.insertCell(3)
     const volMin = row.insertCell(4)
     const volMax = row.insertCell(5)
+    const action = row.insertCell(6)
     nome.innerText = found[0].nome
     cas.innerText = found[0].cas
     vazMin.innerText = found[0].VazMin
     vazMax.innerText = found[0].VazMax
     volMin.innerText = found[0].VolMin
     volMax.innerText = found[0].VolMax
+
+    action.setAttribute('id', 'cleanBtn')
+
+    const button = document.createElement('button')
+    button.innerText = '-'
+    action.appendChild(button)
     
 }
 
-const btnSearch = document.querySelector('button')
+function clean() {
+    const table = document.querySelector('table')
+    const columns = table.querySelectorAll('td')
+    columns.forEach(column => column.remove())
+}
 
-btnSearch.addEventListener('click', () => {
+function cleanRow(target) {
+    target.parentElement.parentElement.remove()
+}
+
+
+document.addEventListener('click', (e) => {
+    const target = e.target
     const searchContent = document.querySelector('#search').value
-    search(searchContent)
+    if(target.innerText === 'Pesquisar') search(searchContent)
+    if(target.innerText === 'Limpar') clean()
+    if(target.innerText === '-') cleanRow(target)
 })
+
